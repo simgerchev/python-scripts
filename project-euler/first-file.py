663c832ccb963d99fae879a8c7fc9271dd1c6e8b
pelasg0 <91074931+pelasg0@users.noreply.github.com>
2025-02-24 18:32:17 +0100

    fibonacci numbers added

--- project-euler/first-file.py
+++ project-euler/first-file.py
@@ -1,7 +1,17 @@
-#Problem 1 
-result = 0 
-for x in range(1, 1000): 
-    if x % 3 == 0 or x % 5 == 0: 
-        result += x 
+def problem_one(): 
+    result = 0 
+    for x in range(1, 1000): 
+        if x % 3 == 0 or x % 5 == 0: 
+            result += x 
+    print(result) 
+    
+#Fibonacci numbers basic
+a = 1
+b = 2
+res = 0
+while res < 40: 
+    res = a + b
+    print(f"{a} + {b} = {res}")
+    a, b = b, res 
 
-print(result) 
+
