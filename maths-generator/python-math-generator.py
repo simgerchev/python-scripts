4bd7ed41d39622d69ee7879825fa84c2dfd41b47
pelasg0 <91074931+pelasg0@users.noreply.github.com>
2025-02-28 21:47:24 +0100

    addition functionality add

--- maths-generator/python-math-generator.py
+++ maths-generator/python-math-generator.py
@@ -1,3 +1,5 @@
+import random
+
 show_menu = True
 
 def start_menu(): 
@@ -5,7 +7,7 @@ def start_menu():
     input_option = input("Which operation do you want to practice? \n"+
                          "1. Addition \n") 
     input_options = {
-            '1': get_addition_task,
+            '1': start_addition_tasks,
             'e': quit_program
     }
     if input_option in input_options: 
@@ -13,8 +15,26 @@ def start_menu():
     else: 
         print("Invalid option. Please choose a valid number.")
 
-def get_addition_task(): 
-    return True
+def start_addition_tasks(): 
+    task_count = int(input("How many tasks do you wanna get? ")) 
+    while task_count>=1: 
+        get_addition_task()
+        task_count-=1
+    return start_menu()
+    
+def get_addition_task():
+    input_min_a = int(input("Smallest number a: "))
+    input_max_a = int(input("Biggest number a: "))
+    input_min_b = int(input("Smallest number b: "))
+    input_max_b = int(input("Biggest number b: "))
+    a = random.randint(input_min_a, input_max_a)
+    b = random.randint(input_min_b, input_max_b)
+
+    answer = input(f"How much is {a} + {b}: ")
+    if int(answer) == a + b: 
+        print("Answer is true") 
+    else: 
+        print("Answer is wrongX") 
 
 def quit_program(): 
     global show_menu
