81c6a01727169bd29069993bf6a08587932310dd
pelasg0 <91074931+pelasg0@users.noreply.github.com>
2025-03-01 23:10:17 +0100

    find_prime method is done

--- project-euler/first-file.py
+++ project-euler/first-file.py
@@ -15,27 +15,14 @@ def problem_two():
         a, b = b, b+a
     print(total)
 
-def find_division_numbers(init_number):
-    i = 2 
-    numbers = []
-    division_numbers = []
-    while i < init_number - 1: 
-        numbers.append(i)
-        i+=1 
-    for num in numbers: 
-        if init_number % num == 0: 
-            division_numbers.append(num)
-    return division_numbers
-
-def find_prime_number(input_number):
+def find_prime(n): 
     prime_numbers_list = []
-    prime_number = input_number
-    while prime_number:
-        numbers_list = find_division_numbers(prime_number)
-        prime_number = numbers_list[0]
-        prime_numbers_list.append(prime_number)
-        find_division_numbers(prime_number)
-    return print(prime_numbers_list)
-
-find_prime_number(100)  
+    div = 2
+    while n > 1: 
+        while n % div == 0: 
+            prime_numbers_list.append(div)
+            n //= div
+        div += 1
+    print(prime_numbers_list)
+find_prime(100)  
 
