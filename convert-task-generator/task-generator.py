b4b4dadd3b7f1d4bf0bb6bad68dea4496512e7b6
pelasg0 <91074931+pelasg0@users.noreply.github.com>
2025-03-10 21:27:45 +0100

    binary to decimal converter is done

--- convert-task-generator/task-generator.py
+++ convert-task-generator/task-generator.py
@@ -2,16 +2,21 @@
 
 def binary_to_decimal(binary_number): 
     """Convert a binary to a decimal number"""
-    is_one = False
     res = 0
     elements = list(binary_number)
-    for element in elements: 
-        if element == '1': 
-            is_one = True
-        elif element == '0': 
-            is_one = False
-        if is_one == True: 
-           res += 1 
+    elements.reverse()
+    elements_val = []
+    
+    i = 0
+
+    while i <= len(elements) - 1: 
+        for element in elements: 
+            if element == '1': 
+                res += 2 ** i
+                i+=1
+            elif element == '0': 
+                res += 0 
+                i+=1
     return print(res) 
 
 binary_to_decimal(input("Give a binary"))
