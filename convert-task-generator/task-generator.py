44746ada4ac1fd3b64f7036ad66150f5993c9a8d
pelasg0 <91074931+pelasg0@users.noreply.github.com>
2025-03-15 22:18:09 +0100

    input length

--- convert-task-generator/task-generator.py
+++ convert-task-generator/task-generator.py
@@ -31,23 +31,28 @@ def binary_to_decimal():
 def exit_app(): 
     return sys.exit()
 
-def generate_task():
+def generate_binary_number():
     bin_number = []
+    bin_number_length = input("Type the length of the binary number: ")
     i = 0
-    while i <= 4: 
+    while i <= bin_number_length: 
         bin_digit = random.randint(0,1) 
         bin_number.append(bin_digit)
+        i+=1 
+    return print(bin_number) 
 
 def start_menu():
     """Display the start menu and handle user input."""
     input_option = input("===== \n"+
                         "What do you want to do? \n"+
                         "1. Convert binary to decimal \n"+
+                        "2. Generate binary number \n"+
                         "e. Quit programm \n"+
                         "===== \n") 
     input_options = {
-            '1': binaryto_to_decimal(),
-            'e': exit_app()
+            '1': binary_to_decimal,
+            '2': generate_binary_number,
+            'e': exit_app
     }
     if input_option in input_options: 
         input_options[input_option]()
@@ -56,3 +61,4 @@ def start_menu():
 
 while True: 
     start_menu()
+
