c1aab0fa58da02b9d1ea64b3f971a87f5e1d1aa8
simgerchev <mrdrawin983@gmail.com>
2025-07-02 11:23:16 +0200

    seventh_task()

---
+++ hacker-rank/README.md
@@ -0,0 +1,3 @@
+```
+Tasks I am solving from the site https://www.hackerrank.com/ so I can get better in python
+```
--- hacker-rank/tasks.py
+++ hacker-rank/tasks.py
@@ -64,5 +64,47 @@ def fourth_task():
     print(a // b)
     print(a / b)
 
+'''
+The provided code stub reads an integer, n , from STDIN. 
+For all non-negative integers i < n , print i ^ 2.
+'''
+def fifth_task():
+    n = int(input())
+    for i in range(0, n): 
+        print(i * i)
 
+'''
+An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar for the fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
 
+In the Gregorian calendar, three conditions are used to identify leap years:
+
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years. 
+'''
+def sixth_task(): 
+    def is_leap(year):
+        leap = False
+        if year % 4 == 0: 
+            leap = True
+            if year % 100 == 0: 
+                leap = False
+                if year % 400 == 0: 
+                    leap = True 
+        
+        return leap
+    year = int(input())
+    print(is_leap(year))
+
+'''
+The included code stub will read an integer, n , from STDIN.
+
+Without using any string methods, try to print the following:
+123...n
+Note that "..." represents the consecutive values in between.
+'''
+def seventh_task(): 
+    n = int(input())
+    for i in range(n):
+        print(i + 1, end='')     
