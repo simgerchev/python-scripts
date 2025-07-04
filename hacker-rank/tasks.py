9e2e40c30891604a86736e0b43b9158fe96a2615
Joe <mrdrawin983@gmail.com>
2025-07-04 08:48:37 +0200

    eighth_task() edit

--- hacker-rank/tasks.py
+++ hacker-rank/tasks.py
@@ -117,9 +117,19 @@ Here, 0 <= i <= x; 0 <= j <= y; 0 <= k <= z.
 Please use list comprehensions rather than multiple loops, as a learning exercise.
 '''
 def eighth_task(): 
-    x, y, z, n = (int(input()) for _ in range(4))
-    print([(i, j, k) for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if (i + j + k) != n])
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
 
+    result = [[i, j, k]
+              for i in range(x + 1)
+              for j in range(y + 1)
+              for k in range(z + 1)
+              if (i + j + k) != n]
+
+    print(result)
+    
 '''
 Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. 
 You are given  scores. 
