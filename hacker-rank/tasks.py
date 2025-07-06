f7539eae39cf3b978fe86fd3befcb08ee87ba277
pelasg0 <91074931+pelasg0@users.noreply.github.com>
2025-07-06 21:49:41 +0200

    eleventh_task

--- hacker-rank/tasks.py
+++ hacker-rank/tasks.py
@@ -187,3 +187,6 @@ def eleventh_task():
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+    student_marks_query = student_marks[query_name]
+    student_mark_avg = sum(student_marks_query)/len(student_marks_query)
+    print("{:.2f}".format(student_mark_avg))
